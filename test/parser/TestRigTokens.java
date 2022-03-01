082cef2711cfa78bcb570a53974669ff1473bffb
sergiomalv <sermual@gmail.com>
2022-03-01 09:18:55 +0100

    Initial material of session 4

---
+++ test/parser/TestRigTokens.java
@@ -0,0 +1,10 @@
+package parser;
+
+public class TestRigTokens {
+	
+	public static void main(String... args) throws Exception {
+		org.antlr.v4.gui.TestRig.main(new String[]{"parser.Pmm", "program", "-tokens", "input.txt"});
+	}
+	
+
+}
---
+++ test/parser/TestRigTrace.java
@@ -0,0 +1,10 @@
+package parser;
+
+public class TestRigTrace {
+	
+	public static void main(String... args) throws Exception {
+		org.antlr.v4.gui.TestRig.main(new String[]{"parser.Pmm", "program", "-trace", "input.txt"});
+	}
+	
+
+}
